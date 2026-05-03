d1020c021b8768a7d2cf1ffe2e5246df801b92a4
Taivo T <tiavooo@gmail.com>
2026-05-03 11:26:47 +0200

    finished

--- cli/lib/cli.dart
+++ cli/lib/cli.dart
@@ -1,3 +1,30 @@
-int calculate() {
-  return 6 * 7;
+import 'package:command_runner/command_runner.dart';
+import 'package:cli/src/logger.dart';
+import 'package:cli/src/commands/search.dart';
+import 'package:cli/src/commands/get_article.dart';
+
+void main(List<String> arguments) async {
+  final errorLogger = initFileLogger('errors');
+  final app =
+      CommandRunner(
+          onOutput: (String output) async {
+            await write(output);
+          },
+          onError: (Object error) {
+            if (error is Error) {
+              errorLogger.severe(
+                '[Error] ${error.toString()}\n${error.stackTrace}',
+              );
+              throw error;
+            }
+            if (error is Exception) {
+              errorLogger.warning(error);
+            }
+          },
+        )
+        ..addCommand(HelpCommand())
+        ..addCommand(SearchCommand(logger: errorLogger))
+        ..addCommand(GetArticleCommand(logger: errorLogger));
+
+  app.run(arguments);
 }
--- command_runner/test/command_runner_test.dart
+++ command_runner/test/command_runner_test.dart
@@ -1,4 +1,1 @@
-import 'package:command_runner/command_runner.dart';
-import 'package:test/test.dart';
 
-void main() {}
